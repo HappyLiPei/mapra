07dd19f8d0953b5f694eb9480bd259c6aac4172d
marie-sophie <ms.friedl@t-online.de>
2015-08-07 10:33:13 +0200

    phenomizer node created

---
+++ MaPraNodes/src/phenomizer/PhenomizerNodeFactory.java
@@ -0,0 +1,58 @@
+package phenomizer;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "Phenomizer" Node.
+ * 
+ *
+ * @author 
+ */
+public class PhenomizerNodeFactory 
+        extends NodeFactory<PhenomizerNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PhenomizerNodeModel createNodeModel() {
+        return new PhenomizerNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<PhenomizerNodeModel> createNodeView(final int viewIndex,
+            final PhenomizerNodeModel nodeModel) {
+        return new PhenomizerNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new PhenomizerNodeDialog();
+    }
+
+}
+
