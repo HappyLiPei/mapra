8e4e1998f56bedf682fa28ac8f98990b7a340d46
marie-sophie <ms.friedl@t-online.de>
2015-08-09 15:10:49 +0200

    update of phenomizer nodemodel

--- MaPraNodes/src/phenomizer/PhenomizerNodeModel.java
+++ MaPraNodes/src/phenomizer/PhenomizerNodeModel.java
@@ -40,6 +40,12 @@ public class PhenomizerNodeModel extends NodeModel {
     // the logger instance
     private static final NodeLogger logger = NodeLogger.getLogger(PhenomizerNodeModel.class);
     
+    //inport positions
+    private static final int INPORT_SYMPTOM_DICT =0;
+    private static final int INPORT_ISA = 1;
+    private static final int INPORT_KSZ = 2;
+    private static final int INPORT_QUERY = 3;
+    
     //column names
     private static final String SYMPTOM_ID ="symptom_id";
     private static final String SYMPTOM_NAME = "symptom_name";
@@ -49,21 +55,16 @@ public class PhenomizerNodeModel extends NodeModel {
     private static final String PARENT_ID = "parent_id";
     private static final String SCORE = "score";
     private static final String P_VALUE = "p_value";
-        
-
 
     /**
      * Constructor for the node model.
+     * 4 inports:
+     * 	-symptom dictionary (0)
+     * 	-isa table (1)
+     * 	-ksz table (2)
+     * 	-query table (3)
      */
     protected PhenomizerNodeModel() {
-    
-    	/**
-    	 * 4 inports:
-    	 * 	-symptom dictionary (0)
-    	 * 	-isa table (1)
-    	 * 	-ksz table (2)
-    	 * 	-query table (3)
-    	 */
         super(4, 1);
     }
 
@@ -120,40 +121,52 @@ public class PhenomizerNodeModel extends NodeModel {
     	
     	logger.info("configure");
     	//check port 0: symptom table
-    	checkColumn(inSpecs, 0, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
-    	checkColumn(inSpecs, 0, SYMPTOM_NAME, StringCell.TYPE, null);
+    	checkColumn(inSpecs, INPORT_SYMPTOM_DICT, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_SYMPTOM_DICT, SYMPTOM_NAME, StringCell.TYPE, null);
     	//check port 1: isa table
-    	checkColumn(inSpecs, 1, CHILD_ID, IntCell.TYPE, LongCell.TYPE);
-    	checkColumn(inSpecs, 1, PARENT_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_ISA, CHILD_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_ISA, PARENT_ID, IntCell.TYPE, LongCell.TYPE);
     	//TODO: check source if required
     	//check port 2: ksz table
-    	checkColumn(inSpecs, 2, DISEASE_ID, IntCell.TYPE, LongCell.TYPE);
-    	checkColumn(inSpecs, 2, DISEASE_NAME, StringCell.TYPE, null);
-    	checkColumn(inSpecs, 2, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_KSZ, DISEASE_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_KSZ, DISEASE_NAME, StringCell.TYPE, null);
+    	checkColumn(inSpecs, INPORT_KSZ, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
     	//check port 3: query  
-    	checkColumn(inSpecs, 3, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
+    	checkColumn(inSpecs, INPORT_QUERY, SYMPTOM_ID, IntCell.TYPE, LongCell.TYPE);
 
         return new DataTableSpec[]{generateOutputSpec()};
     }
+    
+    /**
+     * checks if input specifications from port port contains a column named colname and 
+     * if specifications match data type type1 oder type2
+     * @param inSpecs
+     * @param port
+     * @param colname
+     * @param type1
+     * @param type2
+     * @throws InvalidSettingsException
+     */
     
     private void checkColumn(DataTableSpec[] inSpecs, int port, String colname, DataType type1, DataType type2) throws InvalidSettingsException{
     	
     	DataColumnSpec s =inSpecs[port].getColumnSpec(colname);
     	if(s==null){
-    		throw new InvalidSettingsException("Table at port "+port+" requires column "+SYMPTOM_ID);
+    		throw new InvalidSettingsException("Table at port "+port+" requires column "+colname);
     	}
     	if((s.getType() != type1 && s.getType() != type2)){
-    		throw new InvalidSettingsException("Table at port "+port+": Column "+SYMPTOM_ID+" is not the correct data type");
+    		throw new InvalidSettingsException("Table at port "+port+": Column "+colname+" is not the correct data type");
     	}
     }
     
+	/**
+	 * generates specifications for outport table
+	 * col 0 : disease_id (int)
+	 * col 1 : disease_name (string)
+	 * col 2 : score (double)
+	 * col 3 : p-value (double)
+	 */
     private DataTableSpec generateOutputSpec(){
-    	/**
-    	 * col 0 : disease_id (int)
-    	 * col 1 : disease_name (string)
-    	 * col 2 : score (double)
-    	 * col 3 : p-value (double)
-    	 */
     	DataColumnSpec [] colspecs = new DataColumnSpec [4];
     	colspecs[0] = new DataColumnSpecCreator(DISEASE_ID, IntCell.TYPE).createSpec();
     	colspecs[1] = new DataColumnSpecCreator(DISEASE_NAME, StringCell.TYPE).createSpec();
