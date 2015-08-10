961bc74a3acd0b469eb1e0dd1cf2ccc96e1317da
capre <carolinchen_91@yahoo.de>
2015-08-10 11:25:45 +0200

    TestAlgoCaro

---
+++ MaPraNodes/src/main/TestAlgoCaro.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import algorithm.AlgoPheno;
+
+public class TestAlgoCaro {
+
+	public static void main(String[]args){
+		
+		String dataPath = "C:/Users/Carolin/Dropbox/Masterpraktikum/Testdatensatz/";
+		
+		String diseasesIn = dataPath + "Krankheiten.txt";
+		String symptomsIn = dataPath + "Symptome.txt";
+		String ontologyIn = dataPath + "Ontology.txt";
+		String queryIn = dataPath + "query1.txt";
+
+		HashMap<Integer,LinkedList<Integer>>ksz = FileUtilities.readInKSZ(diseasesIn);
+		LinkedList<Integer> symptoms = FileUtilities.readInSymptoms(symptomsIn);
+		int[][]ontology = FileUtilities.readInOntology(ontologyIn);
+		LinkedList<Integer> query = FileUtilities.readInQuery(queryIn);
+		
+		
+		
+		AlgoPheno.setInput(query, symptoms, ksz, ontology);
+
+	}
+
+	
+	
+	
+}
