452473753db09b0eb0d1ccb775ccc5c850af6569
capre <carolinchen_91@yahoo.de>
2015-08-10 16:03:37 +0200

    TestCaro

--- MaPraNodes/src/main/TestAlgoCaro.java
+++ MaPraNodes/src/main/TestAlgoCaro.java
@@ -37,7 +37,7 @@ public class TestAlgoCaro {
 		*/
 		
 		System.out.println("AllCommonAncestors");
-		int node1 = 13;
+		int node1 = 36;
 		int[] node2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40}; 
 		for(int i = 0; i<40; i++){
 			System.out.println(node1+"\t"+node2[i]+"\t"+onto.getAllCommonAncestors(node1, node2[i]).toString());
