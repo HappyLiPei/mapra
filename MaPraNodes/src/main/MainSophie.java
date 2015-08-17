8f10958e18ad8701db42392272f6f8e35af9f82f
marie-sophie <ms.friedl@t-online.de>
2015-08-17 14:54:58 +0200

    validate OMIM method

--- MaPraNodes/src/main/MainSophie.java
+++ MaPraNodes/src/main/MainSophie.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -86,12 +87,112 @@ public class MainSophie {
 		}
 	}
 	
-	private LinkedList<String [] > executePhenomizer(String file_isa, String file_symptom, String file_ksz, LinkedList<Integer> q){
-		AlgoPheno.setInput(q, FileUtilities.readInSymptoms(file_symptom),
-				FileUtilities.readInKSZ(file_ksz), FileUtilities.readInOntology(file_isa));
-		LinkedList<String[]> res = AlgoPheno.runPhenomizer(20);
-		return res;
+	private LinkedList<String []> readOMIMPheno(String infile){
+		
+		FileInputReader fir = new FileInputReader(infile);
+		String line=fir.read();
+		
+		LinkedList<String []> pheno_to_omim= new LinkedList<String []>();
+		
+		while((line=fir.read())!=null){
+			String [] split = line.split("\t");
+			if(split.length!=3){
+				System.out.println("Format error");
+				continue;
+			}
+			//pos 0: phenodis id, pos 1: omim id
+			String [] id_pair = new String [2];
+			
+			if(!split[2].contains(";")){
+				id_pair[0]=split[0];
+				id_pair[1]=split[2];
+				pheno_to_omim.add(id_pair);	
+			}
+		}
+		return pheno_to_omim;
 	}
+	
+	private double calculateRank(String disease_id, LinkedList<String []> pheno_res){
+		
+		String prev="";
+		int count = 1;
+		boolean found=false;
+		int rank =0;
+		
+		//score[0]: disease_id, score[1]: score
+		for(String [] score: pheno_res){
+			if(!score[1].equals(prev)){
+				if(found){
+					//System.out.println(rank+"\t"+count);
+					break;
+				}
+				rank++;
+				prev=score[1];
+				count=1;
+			}
+			else{
+				rank++;
+				count++;
+			}
+			if(score[0].equals(disease_id)){
+				found=true;
+			}
+			//System.out.println(score[0]+"\t"+score[1]+"\t"+rank+"\t"+count);
+		}
+		
+		if(!found){
+			System.out.println("Error: could not calculate rank");
+		}
+		
+		int sum=0;
+		for(int i=0; i<count; i++){
+			sum+=rank-i;
+		}
+		double d = ((double)sum)/count;
+		//System.out.println(disease_id+" Rang: "+d);
+		return d;
+	}
+	
+	private void runOMIMVal(String outfile, String phenofile, String tmfile, String isa, String symptom, String ksz){
+		
+		// read symptom queries for omim entries extracted with textmiming
+		HashMap<String, LinkedList<Integer>> queries = readQueriesTM(tmfile);
+		//read omim ids and corresponding phenodis disease_ids
+		LinkedList<String []> disease_pairs = readOMIMPheno(phenofile);
+		
+		
+		boolean start = true;
+		int count = 1;
+		Writer_Output w = new Writer_Output(outfile);
+		
+		//pair[0]: pheno_dis pair[1]: omim
+		for(String[] pair :disease_pairs){
+			if(queries.containsKey(pair[1]) && !queries.get(pair[1]).isEmpty()){
+				System.out.println(count+" out of "+ disease_pairs.size());
+				System.out.println("Calculate pair "+pair[1]+"\t"+pair[0]);
+				
+				if(start){
+					AlgoPheno.setInput(queries.get(pair[1]),
+							FileUtilities.readInSymptoms(symptom),
+							FileUtilities.readInKSZ(ksz), FileUtilities.readInOntology(isa));
+					start = false;
+				}
+				else{
+					AlgoPheno.setQuery(queries.get(pair[1]));
+				}
+				LinkedList<String[]> res = AlgoPheno.runPhenomizer(8000);
+				
+				double rank = calculateRank(pair[0], res);
+				System.out.println(pair[0]+"\t"+pair[1]+"\t"+rank);
+				w.writeFileln(pair[0]+"\t"+pair[1]+"\t"+rank);
+				
+			}
+			count++;
+		}
+		w.closew();
+		
+	}
+	
 	
 	
 	public static void main(String args[]){
@@ -100,19 +201,46 @@ public class MainSophie {
 		String isa="/home/marie-sophie/Uni/mapra/phenodis/Datenbank/isa_HPO_test.csv";
 		String ksz="/home/marie-sophie/Uni/mapra/phenodis/Datenbank/ksz_HPO_test.csv";
 		String symptom="/home/marie-sophie/Uni/mapra/phenodis/Datenbank/symptoms_HPO_test.csv";
+		String phenofile ="/home/marie-sophie/Uni/mapra/omim/phenodis_omimids.txt";
+		String outfile="/home/marie-sophie/Uni/mapra/omim/test_out.txt";
 		
 		
 		MainSophie ms = new MainSophie();
-		HashMap<String, LinkedList<Integer>> queries = ms.readQueriesTM(file);
-		
-		//176000 (OMIM) - 6496 (PhenoDis)
-		System.out.println(queries.get("176000"));
-		
-		LinkedList<String[]> res = ms.executePhenomizer(isa, symptom, ksz, queries.get("176000"));
-		for(String []s: res){
-			System.out.println(s[0]+"\t"+s[1]);
-		}
-		
+		ms.runOMIMVal(outfile, phenofile,file, isa, symptom, ksz);
+//		ms.readOMIMPheno(phenofile);
+//		HashMap<String, LinkedList<Integer>> queries = ms.readQueriesTM(file);
+//		
+//		//176000 (OMIM) - 6496 (PhenoDis)
+//		System.out.println(queries.get("176000"));
+//		
+//		int num=1;
+//		LinkedList<String[]> res = ms.executePhenomizer(isa, symptom, ksz, queries.get("176000"));
+//		for(String []s: res){
+//			System.out.println(num+"\t"+s[0]+"\t"+s[1]);
+//			num++;
+//		}
+//		
+//		ms.calculateRank("12036", res);
+//		ms.calculateRank("6496", res);
+//		ms.calculateRank("10890", res);
+//		ms.calculateRank("6495", res);
+//		ms.calculateRank("8868", res);
+//		ms.calculateRank("11763", res);
+//		ms.calculateRank("11762", res);
+//		ms.calculateRank("11741", res);
+//		ms.calculateRank("11396", res);
+//		ms.calculateRank("11337", res);
+//		ms.calculateRank("9465", res);
+//		ms.calculateRank("8916", res);
+//		ms.calculateRank("8772", res);
+//		ms.calculateRank("2789", res);
+//		ms.calculateRank("12044", res);
+//		ms.calculateRank("11854", res);
+//		ms.calculateRank("11842", res);
+//		ms.calculateRank("11547", res);
+//		ms.calculateRank("11399", res);
+//		ms.calculateRank("11217", res);
+//		ms.calculateRank("11160", res);		
 		
 	}
 	
@@ -182,6 +310,54 @@ public class MainSophie {
 		}
 
 
+	}
+	
+	
+	public class  Writer_Output {
+
+		public String path;
+		public BufferedWriter w;
+		
+		public Writer_Output (String path){
+			this.path=path;
+			initiateWriter(path);
+		}
+		
+		public void initiateWriter (String path){
+			try{
+				Charset c = Charset.forName("UTF-8");
+				w = Files.newBufferedWriter(Paths.get(path), c);
+			}
+			catch(IOException e){
+				System.out.println("Error creating the writer for file "+path);
+				System.exit(1);
+			}
+		}
+		
+		public void writeFile(String s){
+			try{
+				w.write(s);
+			}
+			catch(IOException e){
+				System.out.println("Error while writing to file "+path);
+				System.exit(1);
+			}
+		}
+		
+		public void writeFileln(String s){
+			writeFile(s+"\n");
+		}
+		
+		public void closew(){
+			try{
+				w.flush();
+				w.close();
+			}
+			catch(IOException e){
+				System.out.println("Error closing the writer for file "+path);
+			}
+		}
+		
 	}
 
 
