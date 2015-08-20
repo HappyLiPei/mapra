9140f35ee3bcf6b377a356e585eee01d5fb70370
mariasche <mssonstiges@vodafone.de>
2015-08-20 15:18:03 +0200

    update phenomizer

--- MaPraNodes/src/algorithm/MaxPValueComparator.java
+++ MaPraNodes/src/algorithm/MaxPValueComparator.java
@@ -10,10 +10,10 @@ public class MaxPValueComparator implements Comparator<String> {
 		String[]parts2 = s2.split(",");
 
 		if(Integer.valueOf(parts1[0])<Integer.valueOf(parts2[0])){
-			return 1;
+			return -1;
 		}
 		else if(Integer.valueOf(parts1[0])>Integer.valueOf(parts2[0])){
-			return -1;
+			return 1;
 		}
 		else{
 			if(Double.compare(Double.valueOf(parts1[1]), Double.valueOf(parts2[1]))!=0){
