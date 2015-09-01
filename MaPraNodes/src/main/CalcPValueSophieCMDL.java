51e8ecdc06a38907563a3f37f947fd7e215389d6
marie-sophie <ms.friedl@t-online.de>
2015-09-01 12:02:57 +0200

    command line tool to calculate p values

--- MaPraNodes/src/main/CalcPValueSophieCMDL.java
+++ MaPraNodes/src/main/CalcPValueSophieCMDL.java
@@ -13,25 +13,49 @@ public class CalcPValueSophieCMDL {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		int queryLength = Integer.valueOf(args[0]);
 		int iterations = Integer.valueOf(args[1]);
-		String ontoIn = args[2];
-		String kszIn = args[3];
-		String symptomsIn = args[4];
-		String output = args[5];
-		String progress = args[6];
+		String weight =args[2]; // noweight, weight, asymmweight
+		String ontoIn = args[3];
+		String kszIn = args[4];
+		String symptomsIn = args[5];
+		String output = args[6];
+		String progress = args[7];
 		
-		HashMap<Integer,LinkedList<String[]>>kszTmp = FileUtilities.readInKSZFrequency(kszIn);
-		HashMap<Integer,LinkedList<Integer[]>>ksz=FrequencyConverter.convertAll(kszTmp);
+		if(weight.equals("noweight")){
+			System.out.println("not weighted");
+		}
+		else if(weight.equals("weight")){
+			System.out.println("symmetric weight");
+		}
+		else if(weight.equals("asymmweight")){
+			System.out.println("asymmetric weight");
+		}
+		else{
+			throw new Exception("Incorrect argument at pos 2!");
+		}
 		
-//		HashMap<Integer,LinkedList<Integer>>kszTmp = FileUtilities.readInKSZ(kszIn);
-//		HashMap<Integer,LinkedList<Integer[]>>ksz= addWeights(kszTmp);
+		HashMap<Integer, LinkedList<Integer[]>> ksz = new HashMap<Integer, LinkedList<Integer[]>>();
+		if(weight.equals("noweight")){
+			HashMap<Integer,LinkedList<Integer>>kszTmp = FileUtilities.readInKSZ(kszIn);
+			ksz= addWeights(kszTmp);
+		}
+		else{
+			HashMap<Integer,LinkedList<String[]>>kszTmp = FileUtilities.readInKSZFrequency(kszIn);
+			ksz=FrequencyConverter.convertAll(kszTmp);
+		}
 		
 		LinkedList<Integer> symptoms = FileUtilities.readInSymptoms(symptomsIn);
 		int[][]ontology = FileUtilities.readInOntology(ontoIn);
+		
+		boolean symmetry =true;
+		if(weight.equals("asymmweight")){
+			symmetry=false;
+		}
 		
 		LinkedList<Integer>query = new LinkedList<Integer>();
 		AlgoPheno.setInput(query, symptoms, ksz, ontology);
@@ -46,27 +70,18 @@ public class CalcPValueSophieCMDL {
 			disNum++;
 			LinkedList<Double> scores = new LinkedList<Double>();
 			FileUtilities.writeStringToExistingFile(progress, "\n"+disNum+" out of "+ksz.size());
-			for(int i=0; i<iterations; i++){
-//				if(i%10000==0){
-//					FileUtilities.writeStringToExistingFile(progress, i+"\t");
-//				}
-//				int currIter = i+1+(disNum-1)*iterations;
-//				double percentage = (double) currIter/all;
-//				System.out.println(percentage);
-				
+			for(int i=0; i<iterations; i++){				
 				query = calculateQuery(ids,queryLength);
 				AlgoPheno.setQuery(query);
-				double score = AlgoPheno.calculateSymmetricSimilarity(query, ksz.get(disease),false);
+				double score = AlgoPheno.calculateSymmetricSimilarity(query, ksz.get(disease),symmetry);
 				score = score * 100;
 				score = Math.round(score);
 				score = (double)score/100;
 				scores.add(score);
 			}
-			//AlgoPheno.setCalculatedSim();
 			String res = disease+listToString(scores);
 			res = Binner.createString(res.split("\t"));
 			res+="\n";
-			//System.out.println(res);
 			if(disNum==1){
 				FileUtilities.writeString(output, res);
 			}
