6af2735bbe1a2a68f16db5375f5be39f2665c692
marie-sophie <M.friedl@campus.lmu.de>
2016-02-25 11:35:10 +0100

    test for phenomizer (unweighted) with pvalues and phenomizer with limited output size

--- MaPraNodes/test/phenomizeralgorithm/TestPhenomizer.java
+++ MaPraNodes/test/phenomizeralgorithm/TestPhenomizer.java
@@ -20,7 +20,7 @@ public class TestPhenomizer {
 	private HashMap<Integer,LinkedList<Integer[]>> ksz_with_freq;
 	private LinkedList<String> expected_res;
 	
-	private void readData(int i, boolean weight){
+	private void readData(int i, boolean weight, boolean pval){
 		query = FileUtilities.readInQuery("../TestData/Queries/query"+i+".txt");
 		symptoms = FileUtilities.readInSymptoms("../TestData/DiseasesAndSymptoms/symptoms.txt");
 		ontology = FileUtilities.readInOntology("../TestData/DiseasesAndSymptoms/Ontology.txt");
@@ -31,7 +31,13 @@ public class TestPhenomizer {
 				FileUtilities.readInKSZFrequency("../TestData/DiseasesAndSymptoms/ksz_freq.txt"));	
 		
 		if(!weight){
-			expected_res = FileInputReader.readAllLinesFrom("../TestData/ExpectedResults/NoWeightNoP/res_q"+i+".txt");
+			if(!pval){
+				expected_res = FileInputReader.readAllLinesFrom("../TestData/ExpectedResults/NoWeightNoP/res_q"+i+".txt");
+			}
+			else{
+				expected_res = FileInputReader.readAllLinesFrom("../TestData/ExpectedResults/NoWeightP/res_q"+i+".txt");
+				PValueFolder.setPvalFoder("../TestData/PValues");
+			}
 		}
 		else{
 			expected_res = FileInputReader.readAllLinesFrom("../TestData/ExpectedResults/WeightNoP/res_q"+i+".txt");
@@ -43,15 +49,15 @@ public class TestPhenomizer {
 	@Test
 	public void testPhenomizer_NoWeight_NoPVal() {
 		
-		for(int i=1; i<=1; i++){
-			readData(i,false);
+		for(int i=1; i<=10; i++){
+			readData(i,false, false);
 			AlgoPheno.setInput(query, symptoms, ksz_no_freq, ontology);
 			LinkedList<String[]> result = AlgoPheno.runPhenomizer(11,false);
 			for(int j=0; j<=10; j++){
 				String[] elements = expected_res.get(j).split("\t");
-				assertEquals("Query "+i+" result "+j+"does not match expected disease id",
+				assertEquals("Query "+i+" result "+j+" does not match expected disease id",
 						elements[0], result.get(j)[0]);
-				assertEquals("Query "+i+" result "+j+"does not match expected score",
+				assertEquals("Query "+i+" result "+j+" does not match expected score",
 						elements[2], result.get(j)[1]);
 			}
 		}
@@ -60,18 +66,95 @@ public class TestPhenomizer {
 	@Test
 	public void testPhenomizer_Weight_NoPVal() {
 		
-		for(int i=1; i<=1; i++){
-			readData(i,true);
+		for(int i=1; i<=10; i++){
+			readData(i,true, false);
 			AlgoPheno.setInput(query, symptoms, ksz_with_freq, ontology);
 			LinkedList<String[]> result = AlgoPheno.runPhenomizer(11,false);
 			for(int j=0; j<=10; j++){
 				String[] elements = expected_res.get(j).split("\t");
-				assertEquals("Query "+i+" result "+j+"does not match expected disease id",
+				assertEquals("Query "+i+" result "+j+" does not match expected disease id",
 						elements[0], result.get(j)[0]);
-				assertEquals("Query "+i+" result "+j+"does not match expected score",
+				assertEquals("Query "+i+" result "+j+" does not match expected score",
 						elements[2], result.get(j)[1]);
 			}
 		}
+	}
+	
+	@Test
+	public void testPhenomizer_NoWeight_PVal() {
+		
+		for(int i=1; i<=10; i++){
+			readData(i,false, true);
+			LinkedList<String[]> result = PValueGenerator.phenomizerWithPValues(
+					11, query, symptoms, ksz_no_freq, ontology, false);
+			for(int j=0; j<=10; j++){
+				String[] elements = expected_res.get(j).split("\t");
+				assertEquals("Query "+i+" result "+j+" does not match expected disease id",
+						elements[0], result.get(j)[0]);
+				assertEquals("Query "+i+" result "+j+" does not match expected score",
+						elements[2], result.get(j)[1]);
+				assertEquals("Query "+i+" result "+j+" dose not match expected pvalue", 
+						elements[3], result.get(j)[2]);				
+			}
+		}	
+	}
+	
+	@Test
+	public void testPhenomizer_LimitedOutput(){
+		
+		//limited to 4, but output 5 because last 2 elements have the same score
+		readData(5, false, false);
+		expected_res=FileInputReader.readAllLinesFrom(
+				"../TestData/ExpectedResults/ResultSizeLimited/res_q5_noweight_nopval.txt");
+		expected_res.remove(0);
+		AlgoPheno.setInput(query, symptoms, ksz_no_freq, ontology);
+		LinkedList<String[]> result = AlgoPheno.runPhenomizer(4,false);
+		assertEquals("Output size (query 5 imited to 4, no weight, no pvalue) does not match expected output",
+				5, result.size());
+		for(int j=0; j<=4; j++){
+			String[] elements = expected_res.get(j).split("\t");
+			assertEquals("Query 5 (limited to 4, no weight, no pvalue) result "+j+" does not match expected disease id",
+					elements[0], result.get(j)[0]);
+			assertEquals("Query 5 (limited to 4, no weight, no pvalue) result "+j+" does not match expected score",
+					elements[2], result.get(j)[1]);
+		}
+		
+		//limited to 4, expected 4 elements
+		readData(5, true, false);
+		expected_res=FileInputReader.readAllLinesFrom(
+				"../TestData/ExpectedResults/ResultSizeLimited/res_q5_weight_nopval.txt");
+		expected_res.remove(0);
+		AlgoPheno.setInput(query, symptoms, ksz_with_freq, ontology);
+		result = AlgoPheno.runPhenomizer(4,false);
+		assertEquals("Output size (query 5 imited to 4, weight, no pvalue) does not match expected output",
+				4, result.size());
+		for(int j=0; j<=3; j++){
+			String[] elements = expected_res.get(j).split("\t");
+			assertEquals("Query 5 (limited to 4, weight, no pvalue) result "+j+" does not match expected disease id",
+					elements[0], result.get(j)[0]);
+			assertEquals("Query 5 (limited to 4, weight, no pvalue) result "+j+" does not match expected score",
+					elements[2], result.get(j)[1]);
+		}
+		
+		//limited to 4, expected 4 elements, does not consider if last elements have same pvalue
+		readData(5, false, true);
+		expected_res=FileInputReader.readAllLinesFrom(
+				"../TestData/ExpectedResults/ResultSizeLimited/res_q5_noweight_pval.txt");
+		expected_res.remove(0);
+		result = PValueGenerator.phenomizerWithPValues(
+				4, query, symptoms, ksz_no_freq, ontology, false);
+		assertEquals("Output size (query 5 imited to 4, no weight, pvalue) does not match expected output",
+				4, result.size());
+		for(int j=0; j<=3; j++){
+			String[] elements = expected_res.get(j).split("\t");
+			assertEquals("Query 5 (limited to 4, no weight, pvalue) result "+j+" does not match expected disease id",
+					elements[0], result.get(j)[0]);
+			assertEquals("Query 5 (limited to 4, no weight, pvalue) result "+j+" does not match expected score",
+					elements[2], result.get(j)[1]);
+			assertEquals("Query 5 (limited to 4, no weight, pvalue) result "+j+" dose not match expected pvalue", 
+					elements[3], result.get(j)[2]);	
+		}
+		
 	}
 
 }
