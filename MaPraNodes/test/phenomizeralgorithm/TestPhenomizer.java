8497d9221cdab896627d299221a651a4e3138637
marie-sophie <M.friedl@campus.lmu.de>
2016-03-15 11:02:17 +0100

    updated test data for query3 phenomizer weighted

--- MaPraNodes/test/phenomizeralgorithm/TestPhenomizer.java
+++ MaPraNodes/test/phenomizeralgorithm/TestPhenomizer.java
@@ -67,11 +67,8 @@ public class TestPhenomizer {
 	
 	@Test
 	public void testPhenomizer_Weight_NoPVal() {
-		
-		//TODO: calculate manually other queries (1,3)
-		int[] array = new int[] {2,4,5,6,7,8,9,10};
-//		for(int i=1; i<=1; i++){
-		for (int i: array){
+		//TODO: calculate manually other queries (1)
+		for(int i=2; i<=10; i++){
 			readData(i,true, false);
 			PhenomizerDriver d = new PhenomizerDriver(query, symptoms, ksz_with_freq, ontology);
 			d.setPhenomizerAlgorithm(11, false, 1, "");
